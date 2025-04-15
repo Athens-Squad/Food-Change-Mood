6326a077f84bfe5be0a27e9068a7216022bb2266
Shahlaa Haider <zhussain2800@gmail.com>
2025-04-16 00:10:53 +0300

    implementation KmpMatcher class specific search in meal search usecase

--- src/main/kotlin/logic/useCases/mealSearchUseCase/KMPMatcher.kt
+++ src/main/kotlin/logic/useCases/mealSearchUseCase/KMPMatcher.kt
@@ -1,4 +1,26 @@
 package com.thechance.logic.useCases.mealSearchUseCase
 
-class KMPMatcher {
+class KMPMatcher(private val pattern: String) {
+    private val lps: IntArray = buildLPS()
+
+    private fun buildLPS(): IntArray {
+        val lps = IntArray(pattern.length)
+        var length = 0
+        var i = 1
+        while (i < pattern.length) {
+            if (pattern[i] == pattern[length]) {
+                length++
+                lps[i] = length
+                i++
+            } else {
+                if (length != 0) {
+                    length = lps[length - 1]
+                } else {
+                    lps[i] = 0
+                    i++
+                }
+            }
+        }
+        return lps
+    }
 }
