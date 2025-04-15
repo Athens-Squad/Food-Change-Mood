ddf5b1ba89f788dbafcfa8d42119037329f4e670
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-04-15 16:31:14 +0100

    add a use case to get random sweet without eggs

--- src/main/kotlin/di/useCasesModule.kt
+++ src/main/kotlin/di/useCasesModule.kt
@@ -1,5 +1,6 @@
 package com.thechance.di
 
+import com.thechance.logic.useCases.GetRandomSweetWithNoEggsUseCase
 import com.thechance.logic.useCases.GetSeaFoodMealsSortedByProteinContent
 import com.thechance.logic.useCases.SoThinUseCase
 import org.koin.dsl.module
@@ -7,5 +8,6 @@ import org.koin.dsl.module
 val useCasesModule = module {
     single { SoThinUseCase(get()) }
     single { GetSeaFoodMealsSortedByProteinContent(get()) }
+    single { GetRandomSweetWithNoEggsUseCase(get()) }
 
 }
