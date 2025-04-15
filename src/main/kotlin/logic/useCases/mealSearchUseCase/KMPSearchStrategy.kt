53739f4bbcb6a1aee9d47779319b7a869249c315
Shahlaa Haider <zhussain2800@gmail.com>
2025-04-16 00:04:19 +0300

    implementation KmpSearchStrategy class for meal search usecase

--- src/main/kotlin/logic/useCases/mealSearchUseCase/KMPSearchStrategy.kt
+++ src/main/kotlin/logic/useCases/mealSearchUseCase/KMPSearchStrategy.kt
@@ -1,4 +1,10 @@
 package com.thechance.logic.useCases.mealSearchUseCase
 
-class KMPSearchStrategy {
+import com.thechance.model.Meal
+
+class KMPSearchStrategy: MealSearchStrategy {
+    override fun searchMeals(meals: List<Meal>, searchTerm: String): List<Meal> {
+        val matcher = KMPMatcher(searchTerm)
+        return meals.filter { meal -> kmpMatcher.search(meal.name) }
+    }
 }
