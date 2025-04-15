8895830c3ef39db5d5085784ee4a3b56d6c3c222
Shahlaa Haider <zhussain2800@gmail.com>
2025-04-15 23:53:05 +0300

    create MealSearchStrategy interface for meal search usecase

--- src/main/kotlin/logic/useCases/mealSearchUseCase/MealSearchStrategy.kt
+++ src/main/kotlin/logic/useCases/mealSearchUseCase/MealSearchStrategy.kt
@@ -1,4 +1,7 @@
 package com.thechance.logic.useCases.mealSearchUseCase
 
+import com.thechance.model.Meal
+
 interface MealSearchStrategy {
+    fun searchMeals(meals: List<Meal>, searchTerm: String): List<Meal>
 }
