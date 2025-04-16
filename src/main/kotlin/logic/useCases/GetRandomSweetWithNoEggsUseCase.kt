3be69eb657cd85cfd1c2d51f76fafd859c076bff
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-04-16 18:06:01 +0100

    refactor names, filtration logic and delete unecessary cli logic in the use case

--- src/main/kotlin/logic/useCases/GetRandomSweetWithNoEggsUseCase.kt
+++ src/main/kotlin/logic/useCases/GetRandomSweetWithNoEggsUseCase.kt
@@ -3,48 +3,27 @@ package com.thechance.logic.useCases
 import com.thechance.logic.MealsRepository
 import com.thechance.model.Meal
 
-typealias IsUserLikeSweetInput = () -> Boolean?
-typealias ShortDetailMessage = (String, String) -> Unit
-typealias FullDetailMessage = (Meal) -> Unit
-typealias ChoiceMessage = () -> Unit
-
 class GetRandomSweetWithNoEggsUseCase(
     private val mealsRepository: MealsRepository
 ) {
     private val seenSweets = mutableSetOf<Meal>()
 
-    fun swipeOrKeepTheSweet(
-        showNameAndDescription: ShortDetailMessage,
-        showChoiceMessage: ChoiceMessage,
-        showFullDetail: FullDetailMessage,
-        isUseLikeSweet: IsUserLikeSweetInput
-    ) {
-        while (true) {
-            val meal = suggestASweetMeal()
-            seenSweets.add(meal)
-            showNameAndDescription(meal.name, meal.description)
-
-            showChoiceMessage()
-            val isUserLikeIt = isUseLikeSweet() ?: return
-            if (isUserLikeIt) {
-                showFullDetail(meal)
-                return
-            } else {
-                suggestASweetMeal()
-            }
+    fun suggestASweetMeal(): Meal? {
+        return try {
+            getSweetMealsWithoutEggsOnly()
+                .takeWhile(::isNotInSeenSweets)
+                .random()
+                .also { seenSweets.add(it) }
+        } catch (e: NoSuchElementException) {
+           return null  // No sweets without eggs left
         }
-    }
-
-    private fun suggestASweetMeal(): Meal {
-        return getSweetMealsOnly()
-            .random()
-            .takeIf(::isNotInSeenSweets) ?: suggestASweetMeal()
     }
 
     private fun isNotInSeenSweets(meal: Meal): Boolean {
         return meal !in seenSweets
     }
-    private fun getSweetMealsOnly(): List<Meal> {
+
+    private fun getSweetMealsWithoutEggsOnly(): List<Meal> {
         return mealsRepository.getAllMeals()
             .filterNotNull()
             .filter(::onlySweetsWithoutEggs)
