1b2f724c32ccf80359e5fcdd2b0665018309f2a0
Mohamed Hussein <mohussein1279@gmail.com>
2025-04-16 21:18:37 +0200

    fix usecases module conflict

--- src/main/kotlin/di/useCasesModule.kt
+++ src/main/kotlin/di/useCasesModule.kt
@@ -1,10 +1,6 @@
 package com.thechance.di
 
-import com.thechance.logic.useCases.GetRandomSweetWithNoEggsUseCase
-import com.thechance.logic.useCases.GetSeaFoodMealsSortedByProteinContent
-import com.thechance.logic.useCases.SearchByCountryName
-import com.thechance.logic.useCases.SoThinUseCase
-import com.thechance.logic.useCases.MealPrepTimeGuessGameUseCase
+import com.thechance.logic.useCases.*
 import org.koin.dsl.module
 
 val useCasesModule = module {
@@ -13,5 +9,6 @@ val useCasesModule = module {
     single { GetSeaFoodMealsSortedByProteinContent(get()) }
     single { SearchByCountryName(get()) }
     single { GetRandomSweetWithNoEggsUseCase(get()) }
+    single { GymHelperUseCase(get())}
 
 }
