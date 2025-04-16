14cd930fd9382fdc27cf779e4b02c3f23a879419
Shahlaa Haider <zhussain2800@gmail.com>
2025-04-16 22:45:38 +0300

    add dependency injection for class meal search and get healthy food

--- src/main/kotlin/di/useCasesModule.kt
+++ src/main/kotlin/di/useCasesModule.kt
@@ -1,11 +1,15 @@
 package com.thechance.di
 
+import com.thechance.logic.useCases.GetHealthyMealsUseCase
 import com.thechance.logic.useCases.GetSeaFoodMealsSortedByProteinContent
 import com.thechance.logic.useCases.SoThinUseCase
+import com.thechance.logic.useCases.mealSearchUseCase.MealSearchUseCase
 import org.koin.dsl.module
 
 val useCasesModule = module {
     single { SoThinUseCase(get()) }
     single { GetSeaFoodMealsSortedByProteinContent(get()) }
+    single { GetHealthyMealsUseCase(get()) }
+    single { MealSearchUseCase(get()) }
 
 }
