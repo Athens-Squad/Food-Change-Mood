1686d045575f176a31a7e480e3418567c746f91d
Mohamed Hussein <mohussein1279@gmail.com>
2025-04-16 20:01:40 +0200

    modify search by country name use case

--- src/main/kotlin/logic/useCases/SearchByCountryName.kt
+++ src/main/kotlin/logic/useCases/SearchByCountryName.kt
@@ -2,18 +2,23 @@ package com.thechance.logic.useCases
 
 import com.thechance.logic.MealsRepository
 import com.thechance.model.Meal
+import com.thechance.util.Nationalities.countryToNationality
 
 class SearchByCountryName(private val repository: MealsRepository) {
 
     fun getMealsByCountry(country: String): List<Meal> {
         val lowerCaseCountry = country.lowercase()
+        val nationality = countryToNationality[lowerCaseCountry].toString()
         return repository.getAllMeals()
             .filterNotNull()
             .filter { meal ->
-                meal.tags.any { tag ->
+                meal.tags.any {tag ->
                     tag.lowercase() == lowerCaseCountry
+                    ||tag.lowercase() == nationality
                 }
-                meal.description.contains(lowerCaseCountry)
+                    ||meal.description.contains(lowerCaseCountry)
+                    ||meal.description.contains(nationality)
+                    ||meal.name.contains(nationality)
             }
             .shuffled()
             .take(20)
