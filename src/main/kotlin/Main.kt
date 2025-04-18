830e4e3265f2951dddfbc4eaff7f9f0929151faf
am17jx <ameerahmed0780@gmail.com>
2025-04-18 12:44:43 +0300

    Add main code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 package com.thechance
 
+import com.thechance.di.appModule
+import com.thechance.di.useCasesModule
+import com.thechance.logic.useCases.SuggestItalianMealsForLargeGroupsUseCase
+import org.koin.core.context.startKoin
+import org.koin.java.KoinJavaComponent.getKoin
+
 fun main() {
-    println("Hello World!")
+    startKoin {
+        modules(appModule, useCasesModule)
+    }
+
+    val instance: SuggestItalianMealsForLargeGroupsUseCase = getKoin().get()
+
+    instance.suggestItalianMealsForLargeGroups().forEachIndexed { index, meal ->
+        println("${index + 1}. ${meal.name}")
+    }
 }
